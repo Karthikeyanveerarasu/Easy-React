c554026bcd5de0af9a4e162ef19e7c2369d0c10a
KARTHIKEYAN V V <111226561+Karthikeyanveerarasu@users.noreply.github.com>
2023-01-01 06:59:01 +0530

    Add files via upload
    TypeWriter Effect in React . . .

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import react from 'react';
+import Typewriter from 'typewriter-effect';
+export default function App() {
+  return (
+      <div className="App">  
+      <h1>Hi I'm</h1>{' '}
+      <span style={{color:"red",fontSize:"5vmin",marginBottom:"20px",fontWeight:"bold"}}>
+        <Typewriter
+       options={{
+         autoStart:true,
+         loop:true,
+         delay:40,
+         strings:[
+           "Karthikeyan V V .",
+           "Full Stack Developer .",
+           "Free Lancer ."
+         ]
+       }
+       }
+         Strings={[
+           "KARTHIKEYAN V V",
+         ]}
+         
+
+        />
+
+      </span>
+      </div>
+   
+  );
+}
