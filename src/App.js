4fe0312d02cb57a9d8cf16201a55b904f919f8f5
KARTHIKEYAN V V <111226561+Karthikeyanveerarasu@users.noreply.github.com>
2023-01-08 09:53:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,3 +32,36 @@ export default function App() {
    
   );
 }
+
+
+2.Parellex Effect. . . . 
+
+///// Parellex Effect. . . . 
+import react from 'react';
+import {Parallax} from 'react-parallax';
+const img1="https://images.pexels.com/photos/1379630/pexels-photo-1379630.jpeg?auto=compress&cs=tinysrgb&w=600";
+const inlineStyle ={
+  background:'#fff',
+  left:'50%',
+  top:'50%',
+  position:'absolute',
+  padding:'20px',
+  transform:'translate(-50%,-50%)',
+}
+export default function App() {
+  return (
+    <div className="App">
+    <Parallax bgImage={img1} strength={500}>
+      <div style={{height:500}}>
+ <div style={inlineStyle}>
+        Learn More
+      </div>
+
+      </div>
+     
+      </Parallax>
+      <div style={{height:'100vh'}}></div>
+    </div>
+  );
+}
+
